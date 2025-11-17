1280cb0db1acdc2382d5a05c313f513ff8ead8d7
nguyenvu-ops <nguyenvu@woody.vn>
2025-11-17 13:32:43 +0700

    Update settings.js

--- api/settings.js
+++ api/settings.js
@@ -1,1 +1,6 @@
-// settings endpoint
+import { getSettings } from "../utils/sheets.js";
+
+export default async function handler(req, res) {
+  const data = await getSettings();
+  res.json(data);
+}
