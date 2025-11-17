6166834edbf57a694f043a2aa97413c816fac636
nguyenvu-ops <nguyenvu@woody.vn>
2025-11-17 13:34:41 +0700

    Update cache.js

--- utils/cache.js
+++ utils/cache.js
@@ -1,1 +1,27 @@
-// cache logic
+let cache = {};
+let cacheExpiry = {};
+
+export function getCache(key) {
+  if (!cache[key]) return null;
+  if (Date.now() > cacheExpiry[key]) {
+    delete cache[key];
+    delete cacheExpiry[key];
+    return null;
+  }
+  return cache[key];
+}
+
+export function setCache(key, value, ttl = 600000) {
+  cache[key] = value;
+  cacheExpiry[key] = Date.now() + ttl;
+}
+
+export function clearCache(key) {
+  delete cache[key];
+  delete cacheExpiry[key];
+}
+
+export function clearAllCache() {
+  cache = {};
+  cacheExpiry = {};
+}
