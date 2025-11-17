e3a02ee82836dd1d1242520642a2c8b20931d463
nguyenvu-ops <nguyenvu@woody.vn>
2025-11-17 13:32:02 +0700

    Update hardware.js

--- api/hardware.js
+++ api/hardware.js
@@ -1,1 +1,12 @@
-// hardware endpoint
+import { getHardware } from "../utils/sheets.js";
+
+export default async function handler(req, res) {
+  let data = await getHardware();
+
+  const { brand, category } = req.query;
+
+  if (brand) data = data.filter(x => x.BRAND === brand);
+  if (category) data = data.filter(x => x.CATEGORY === category);
+
+  res.json(data);
+}
